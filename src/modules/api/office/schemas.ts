06557fd4fdf91e702d8eb5e306266cf449ac8f06
Franco Grobler <franco@grobler.fyi>
2025-03-16 21:55:27 +0200

    feat/storage: create basic schemas

---
+++ src/modules/api/office/schemas.ts
@@ -0,0 +1,18 @@
+import type { StaffSchema } from "@/modules/api/staff/schemas";
+
+/* Office schema used for list data */
+export type OfficeSchema = {
+  id: number;
+  name: string;
+  address: string;
+  emailAddress: string;
+  phone: string;
+  capacity: number;
+  colour: string;
+  staffCount: number;
+};
+
+/* Full office schema */
+export type DetailOfficeSchema = {
+  staff: Array<StaffSchema>;
+} | OfficeSchema;
