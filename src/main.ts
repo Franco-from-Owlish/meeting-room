4fb31023d769fda117d8808feae90b094924b4bb
Franco Grobler <franco@grobler.fyi>
2025-03-16 17:42:23 +0200

    feat/vuetify: rework plugins, register vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 
-import App from "./App.vue";
-import router from "./router";
+import App from "@/App.vue";
+import pinia from "@/stores";
+import router from "@/router";
+import vuetify from "@/modules/vuetify";
 
 const app = createApp(App);
 
-app.use(createPinia());
+// Register plugins
+app.use(pinia);
 app.use(router);
+app.use(vuetify);
 
 app.mount("#app");
