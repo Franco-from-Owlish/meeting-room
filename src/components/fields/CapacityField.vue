7d3a9a2062114f7b3c0259cdfebd60dbe0bd4d39
Franco Grobler <franco@grobler.fyi>
2025-03-17 23:36:15 +0200

    feat/landing-page: add capacity field

---
+++ src/components/fields/CapacityField.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-text-field v-model="computedModel" placeholder="Maximum capacity" label="Maximum capacity" suffix="Staff Members"
+    type="number">
+  </v-text-field>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { VTextField } from "vuetify/components/VTextField";
+
+const model = defineModel<number>({ required: true });
+const computedModel = computed<number | undefined>({
+  get: () => (model.value > 0 ? model.value : undefined),
+  set: (value: string | number | undefined) => {
+    if (typeof value === "string") {
+      value = parseInt(value);
+    }
+    model.value = value == undefined || value < 0 ? 0 : value;
+  },
+});
+</script>
