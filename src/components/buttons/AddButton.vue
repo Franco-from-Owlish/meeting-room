38067122b2be2721056f20812c66ec1f9c02c047
Franco Grobler <franco@grobler.fyi>
2025-03-17 22:09:21 +0200

    feat/landing-page: create add button

---
+++ src/components/buttons/AddButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-fab
+    app
+    class="mr-4"
+    color="#0D4477"
+    :icon="mdiPlus"
+    @click="showAddModal"
+    :height="size"
+    :width="size"
+  />
+</template>
+
+<script setup lang="ts">
+  import { VFab } from "vuetify/components/VFab";
+  import { mdiPlus } from "@mdi/js";
+
+  import { useAppStore } from "@/stores/app";
+
+  const size: string = "64px";
+
+  const appStore = useAppStore();
+
+  function showAddModal() {
+    appStore.showModal("add");
+  }
+</script>
