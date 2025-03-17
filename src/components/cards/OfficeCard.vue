410ebce1d9bad895ef0334d207781ec8814a0ac6
Franco Grobler <franco@grobler.fyi>
2025-03-17 21:38:55 +0200

    feat/landing-page: create office card

---
+++ src/components/cards/OfficeCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card>
+    <template #title>
+      {{ office.name }}
+    </template>
+
+    <template #text>
+      <v-row>
+        <PeopleIcon />
+        <span id="staff-detail">
+          <span id="staff-count-detail">
+            {{ office.staffCount }}
+          </span>
+          Staff members in office
+        </span>
+      </v-row>
+    </template>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import type { OfficeSchema } from "@/modules/api/office/schemas";
+import { VCard } from "vuetify/components/VCard";
+import { VRow } from "vuetify/components/VGrid";
+import PeopleIcon from "@/components/icons/PeopleIcon.vue";
+
+defineProps<{
+  office: OfficeSchema;
+}>();
+</script>
