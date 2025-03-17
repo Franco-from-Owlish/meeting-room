9028ff1b04cf82f18810c4fab8b6d22537eb9b75
Franco Grobler <franco@grobler.fyi>
2025-03-17 21:37:37 +0200

    feat/landing-page: add modal app states

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,8 +1,34 @@
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { defineStore } from "pinia";
+import { useRoute, useRouter } from "vue-router";
 
 export const useAppStore = defineStore("app", () => {
+  const route = useRoute();
+  const router = useRouter();
+
+  // page title
   const pageTitle = ref<string>("All offices");
 
-  return { pageTitle };
+  // modals
+  function showModal(queryKey: "add" | "staff") {
+    router.push({
+      path: route.path,
+      query: {
+        add: queryKey === "add" ? "show" : undefined,
+        staff: queryKey === "staff" ? "show" : undefined,
+      },
+    });
+  }
+  const showAddModal = computed<boolean>(() => route.query["add"] === "show");
+  const showStaffModal = computed<boolean>(() => route.query["staff"] === "show");
+
+  return {
+    // pageTitle
+    pageTitle,
+
+    // modals
+    showAddModal,
+    showStaffModal,
+    showModal,
+  };
 });
