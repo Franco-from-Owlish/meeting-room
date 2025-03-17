dd1a01e88fef1fe2fe215ae5265edbcaeaba978c
Franco Grobler <franco@grobler.fyi>
2025-03-17 21:39:03 +0200

    feat/landing-page: update home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
-<script setup lang="ts">
-  import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <OfficeCard v-for="office in offices" :key="office.id" :office="office" />
+  </div>
 </template>
+
+<script setup lang="ts">
+import OfficeCard from "@/components/cards/OfficeCard.vue";
+import OfficeApi from "@/modules/api/office";
+import type { OfficeSchema } from "@/modules/api/office/schemas";
+import { onMounted, ref } from "vue";
+
+const officeApi = new OfficeApi();
+const offices = ref<Array<OfficeSchema>>([]);
+
+async function fetchOffices() {
+  offices.value = await officeApi.getAllOffices();
+}
+
+onMounted(() => {
+  fetchOffices();
+});
+</script>
