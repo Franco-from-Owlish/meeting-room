b375f8f83221fb1d64351b6a802060a0b8d0aca3
Franco Grobler <franco@grobler.fyi>
2025-03-17 23:16:10 +0200

    feat/landing-page: add colours

--- src/modules/vuetify/lightTheme.ts
+++ src/modules/vuetify/lightTheme.ts
@@ -7,6 +7,8 @@ const lightTheme: ThemeDefinition = {
     "on-primary": "#F8FAFC",
     "surface": "#F8FAFC",
     "on-surface": "#000000",
+    "primary-dark": "#0D4477",
+    "on-primary-dark": "#F8FAFC",
   },
 };
 
