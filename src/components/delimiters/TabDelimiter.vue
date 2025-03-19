14609ea9dde2d182ecff508702edb916bc01b4f1
Franco Grobler <franco@grobler.fyi>
2025-03-19 02:02:37 +0200

    feat/office-details: add tab delimiter

---
+++ src/components/delimiters/TabDelimiter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex justify-center ga-1">
+    <v-icon
+      v-for="idx in Array(count).keys()"
+      :key="idx"
+      :icon="idx === model ? mdiCircleOutline : mdiCircle"
+      size="8"
+      :color="colour"
+    ></v-icon>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { mdiCircle, mdiCircleOutline } from "@mdi/js";
+  import { VIcon } from "vuetify/components/VIcon";
+
+  const model = defineModel<number>({ required: true });
+  withDefaults(defineProps<{ count: number; colour?: string }>(), { colour: "primary" });
+</script>
