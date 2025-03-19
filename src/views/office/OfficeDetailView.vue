7ea3aec99515270fa5f97dd44d08ec1a78e97cd9
Franco Grobler <franco@grobler.fyi>
2025-03-19 22:07:26 +0200

    feat/office-details: basic detail view complete

--- src/views/office/OfficeDetailView.vue
+++ src/views/office/OfficeDetailView.vue
@@ -15,21 +15,48 @@
       <OfficeCard :office="office"></OfficeCard>
 
       <AddOfficeStaffDialog :office-id="officeId"></AddOfficeStaffDialog>
+
+      <v-text-field
+        v-model="search"
+        label="Search"
+        placeholder="Jane Doe"
+        :append-inner-icon="mdiMagnify"
+        class="mt-4"
+      ></v-text-field>
+
+      <div class="d-flex align-center">
+        <SectionTitile class="flex-grow-1">Staff Members in Office</SectionTitile>
+        <span>{{ filteredStaff.length }}</span>
+      </div>
+
+      <v-list bg-color="#FFF0">
+        <StaffItem
+          v-for="staff in filteredStaff"
+          :key="staff.id"
+          :member="staff"
+          class="mb-2"
+        ></StaffItem>
+      </v-list>
     </template>
   </div>
 </template>
 
 <script setup lang="ts">
   import { computed, onMounted, ref, watch } from "vue";
+  import { mdiMagnify } from "@mdi/js";
   import { useRoute } from "vue-router";
   import { VRow } from "vuetify/components/VGrid";
+  import { VList } from "vuetify/components/VList";
   import { VProgressCircular } from "vuetify/components/VProgressCircular";
+  import { VTextField } from "vuetify/components/VTextField";
 
   import OfficeCard from "@/components/cards/OfficeCard.vue";
   import AddOfficeStaffDialog from "@/components/dialogs/AddOfficeStaffDialog.vue";
   import SectionTitile from "@/components/headings/SectionTitile.vue";
+  import StaffItem from "@/components/lists/StaffItem.vue";
   import OfficeApi from "@/modules/api/office";
-  import type { OfficeSchema } from "@/modules/api/office/schemas";
+  import type { OfficeDetailSchema } from "@/modules/api/office/schemas";
+  import type { StaffSchema } from "@/modules/api/staff/schemas";
   import { useAppStore } from "@/stores/app";
 
   const appStore = useAppStore();
@@ -41,7 +68,15 @@
   });
 
   const officeId = computed<number>(() => parseInt((route.params["id"] as string) ?? "0"));
-  const office = ref<OfficeSchema>();
+  const office = ref<OfficeDetailSchema>();
+
+  const search = ref<string>("");
+  const filteredStaff = computed<StaffSchema[]>(() => {
+    if (office.value == undefined) return [];
+    return office.value.staff.filter(({ firstName, lastName }) =>
+      `${firstName} ${lastName}`.toLowerCase().includes(search.value.toLowerCase()),
+    );
+  });
 
   async function fetchOffice() {
     const resp = await api.getOffice(officeId.value);
