19e98956ec36d947f3023f9309c8ce36f46d9517
Franco Grobler <franco@grobler.fyi>
2025-03-19 21:46:35 +0200

    feat/office-details: add staff list item

---
+++ src/components/lists/StaffItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-list-item>
+    <template #prepend>
+      <v-avatar :image="StaffAvatars[member.avatar]"></v-avatar>
+    </template>
+
+    <template #title> {{ member.firstName }} {{ member.lastName }} </template>
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+  import { VAvatar } from "vuetify/components/VAvatar";
+  import { VListItem } from "vuetify/components/VList";
+
+  import StaffAvatars from "@/data/avatars";
+  import { type StaffSchema } from "@/modules/api/staff/schemas";
+
+  type VListItemProps = /* @vue-ignore */ VListItem["$props"];
+
+  defineProps<VListItemProps & { member: StaffSchema }>();
+</script>
